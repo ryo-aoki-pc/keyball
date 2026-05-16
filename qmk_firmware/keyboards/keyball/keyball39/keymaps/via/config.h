1b14e6db0c1db6ec0b585997c7a641b4fe5db6fe
Ryo Aoki <ryo.aoki.pc@gmail.com>
2026-05-17 00:48:24 +0900

    Keyboard Quantizer対応

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
@@ -33,6 +33,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#    define RGBLIGHT_EFFECT_TWINKLE
 #endif
 
+#define SPLIT_USB_TIMEOUT 2500
+
 #define TAP_CODE_DELAY 5
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
